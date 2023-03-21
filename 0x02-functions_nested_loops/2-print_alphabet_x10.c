66112f5792754458429ff35ce706687c0f199fc3
mamohsen <mamohsen@aucegypt.edu>
2023-03-21 16:27:52 -0700

    updating 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,12 +6,14 @@
  */
 void print_alphabet_x10(void)
 {
-    for (int i = 0; i < 10; ++i)
+    int i, n;
+
+    for (i = 0; i < 10; ++i)
     {
-        for (int n= 'a'; n<= 'z'; n++)
+        for (n= 'a'; n<= 'z'; n++)
         {
             _putchar(n);
         }
-        _putchar ('\n');
     }
+     _putchar ('\n');
 }
