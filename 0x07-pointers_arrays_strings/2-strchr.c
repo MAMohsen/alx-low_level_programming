9fcb38d17bfab440041aeb3b26aea5fab08889f1
MAMohsen <MAMohsen@AUCEgypt.edu>
2023-04-04 17:20:14 +0200

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,6 +7,7 @@
  *Return: the pointer to the location of the first occurence of the character c in string s.
  *Return: NULL if character is not found.
  */
+
 char *_strchr(char *s, char c)
 {
 	while (*s)
