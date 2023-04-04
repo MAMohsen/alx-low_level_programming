898e6930f71e866e2d1a2b82ec6917d481e3ae3d
MAMohsen <MAMohsen@AUCEgypt.edu>
2023-04-04 16:44:22 +0200

    Updating all files

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- * _memset - fills memory with a constant byte.
+ * _memset - fills memory with a constant byte,
  * @s: memory area.
  * @b: constant byte.
- * @n: bytes files
- * Return: the pointer to destination.
+ * @n: bytes filled.
+ * Return: the pointer to dest.
  */
-char *_memset(char *s, char b, unsigined int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < b; i++)
-		*(s+i) =b;
+	for (i = 0; i < n; i++)
+		*(s + i) =  b;
 
 	return (s);
 }
--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,17 +1,17 @@
-#include "main.h"
+#include "holberton.h"
 /**
- * _memcpy - copies memory data from src to dest.
- * @dest: destination memory area (memory area where to copy to).
- * @src: source memory area (the memory area to copy from).
- * @n: the number of bytes to be copied.
- * Return: the pointer to the destination.
+ * _memcpy - copies memory area,
+ * @dest: destination memory area.
+ * @src: source memory area.
+ * @n: bytes filled.
+ * Return: the pointer to dest.
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	usigned int i;
+	unsigned int i;
 
-	for (i = 0; i < n ; i++)
-		*(dest+i) = *(src+i);
-	
-	return(dest);
+	for (i = 0; i < n; i++)
+		*(dest + i) =  *(src + i);
+
+	return (dest);
 }
